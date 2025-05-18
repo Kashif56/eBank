b7162277d86fde66ece97a8c578dd1e910434d4e
Kashif56 <8bpcoins4u@gmail.com>
2025-05-17 23:07:54 -0700

    chore: initialize Django project with core settings and deployment configuration

--- online_bank/settings.py
+++ online_bank/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5l^5x5#)1k^)1h&$1x5h1x5h1x5h1x5h1x5h1x5h1x5h1x5h1x5h1x5h1x5h1x5h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DJANGO_DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ['*']
 
@@ -84,11 +84,18 @@ ASGI_APPLICATION = 'online_bank.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+if DEBUG == False:
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
 }
 
 
@@ -108,6 +115,13 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ebank.up.railway.app',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 
 
